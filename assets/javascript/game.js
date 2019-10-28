670d36b348bbbc6bbc8eccd793d342eb2aabea5c
J21 <jonster1992@gmail.com>
2019-10-27 20:24:25 -0500

    Finished fixing the game!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,7 @@
-(function() { // Wrap in a function to not pollute the global scope
-
-  // Colors. These are the color names you can use in CSS. You could use color codes
-  // like #rrggbb as well.
-  var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
-
-  var colorIndex = 0;
-
-  setInterval(function(){
-    // Set the color and increment the index.
-    document.body.style.backgroundColor = colors[colorIndex++];
-
-    // Wrap the index if it goes past the length of the array. % is modulo.
-    colorIndex %= colors.length;
-    }, 1000);
-})();
 alert("Hi! Welcome to the game! Let's play!!");
+	//this first variable holds the array of letters
 	var Choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-	var wins = 0;
+	var wins = 0; //initialise the wins, losses, and guesses remaining: 
 		losses = 0;
 		guesses = 9;
 		var gameDiv = document.getElementById("game");
@@ -24,8 +9,13 @@ alert("Hi! Welcome to the game! Let's play!!");
 		var guessArr = [];
 		var i = 0;
 		var j = 0;
+		/*When a key is pressed, this function will begin.
+		The computer will select a key from the Choices array at random and compare
+		against the key you pressed. If correct, you win! Otherwise, you keep
+		guessing until you run out.
+		*/
 		document.onkeyup = function(event){
-		computerChoice = Choices[Math.floor(Math.random() * Choices.length)];
+		computerChoice = Choices[Math.floor(Math.random() * Choices.length)]; //chooses a random letter from the Choices array
 		guessesDiv.innerHTML = "Your guesses so far: ";
 		var userGuess = event.key;
 			for(i = 0; i < Choices.length; i++){
@@ -36,9 +26,9 @@ alert("Hi! Welcome to the game! Let's play!!");
 			if(userGuess === computerChoice) {
 				wins++;
 				guesses = 9;
-				for(j = 0; j < guessArr.length; j++){
-					guessArr[j] = null;
-				}
+				guessArr = [];
+				guessesDiv.innerHTML = "Your guesses so far: ";
+				j = 0;
 			} else if (userGuess !== computerChoice) {
 				guesses--;
 				guessArr[j] = userGuess;
@@ -48,15 +38,16 @@ alert("Hi! Welcome to the game! Let's play!!");
 			if( guesses === 0){
 				losses++;
 				guesses = 9;
-				for(j = 0; j <guessArr.length; j++){
-					guessArr[j] = null;
-				}
+				guessArr = [];
+				guessesDiv.innerHTML = "Your guesses so far: ";
+				j = 0;
 			}
 		var html = 
 		"<p>You chose: " + userGuess + "</p>" +
         "<p>The computer chose: " + computerChoice + "</p>" +
         "<p>wins: " + wins + "</p>" +
-        "<p>losses: " + losses + "</p>";
+        "<p>losses: " + losses + "</p>" + 
+        "<p>guesses: " + guesses + "</p>";
         gameDiv.innerHTML = html;
       
 }
